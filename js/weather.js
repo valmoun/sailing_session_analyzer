2f89db495000944bb17d2c3dd0a21892aa47cfb8
Valentin Mounier <97962665+valmoun@users.noreply.github.com>
2026-08-01 11:43:06 +0200

    Adding automatic weather fetching logic.

---
+++ js/weather.js
@@ -0,0 +1,13 @@
+const openMeteoURL = 
+
+// Getting session reference point as gps coordinates for the weather fetch
+// Fetches the gps epicenter from geo.js
+
+// Getting the session date and time (at the middle of the session)
+
+
+// Fetching average wind and wind gust speed and orientation from open-meteo API
+
+// Returning function that returns for an input session
+// - wind gust speed and orientation
+// - avg wind speed and orientation
